93ce349c079eeafd33aa8be7945174a50ad9549b
Alejandra Castillo <ahle.castillo@gmail.com>
2018-05-24 16:41:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,3 @@ var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 console.log(message)
-var message = `${name} can't do without ${height} inches of dick`
-console.log(message)
